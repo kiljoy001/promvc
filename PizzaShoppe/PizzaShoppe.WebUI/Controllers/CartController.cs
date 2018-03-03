83aeaee8bd824777da4a869d42dd2ff3a6ff3010
kiljoy001 <kiljoy001@gmail.com>
2018-03-03 10:30:18 -0800

    added gsdk, images, updated list to work with adding cart items, fixed layout to use transparent navbar with color transition on scroll, added model cart index view model. Started main index page controller,

---
+++ PizzaShoppe/PizzaShoppe.WebUI/Controllers/CartController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PizzaShoppe.Domain.Concrete;
+using PizzaShoppe.Domain.Entities;
+using PizzaShoppe.WebUI.Models;
+
+namespace PizzaShoppe.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private pizzaEntities Context;
+
+        public CartController(pizzaEntities context)
+        {
+            Context = context;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            MenuItem item = Context.MenuItems.FirstOrDefault(i => i.ProductID == productId);
+            if (item != null)
+            {
+                GetCart().AddItem(item, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        
+        public RedirectToRouteResult RemoveFromCart(int productid, string returnUrl)
+        {
+            MenuItem item = Context.MenuItems.FirstOrDefault(i => i.ProductID == productid);
+            if(item != null)
+            {
+                GetCart().RemoveLine(item);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
--- PizzaShoppe/PizzaShoppe.WebUI/Controllers/OrderController.cs
+++ PizzaShoppe/PizzaShoppe.WebUI/Controllers/OrderController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using PizzaShoppe.Domain.Concrete;
-using System.Data.Entity;
 
 
 namespace PizzaShoppe.WebUI.Controllers
@@ -22,17 +18,14 @@ namespace PizzaShoppe.WebUI.Controllers
         // GET: Order
         public ActionResult List(pizzaEntities context)
         {
-            //Create linked list to pass to the view
-            LinkedList<MenuItem> linkedList = new LinkedList<MenuItem>();
-            //Cycle through the menu items and add them to the linked list
-            foreach(var item in context.MenuItems)
+            //Create list to pass to the view
+            List<MenuItem> MList = new List<MenuItem>();
+            //Cycle through the menu items and add them to the list
+            foreach (var item in context.MenuItems)
             {
-                //Create the node
-                LinkedListNode<MenuItem> menuNode = new LinkedListNode<MenuItem>(item);
-                //Add the node to the end of the list - linkedList will be in the same order as the context.
-                linkedList.AddLast(menuNode);
+                MList.Add(item);
             }
-            return View(linkedList);
+            return View(MList);
         }
     }
 }
